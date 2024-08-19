e8f8db38862e935cf2b969dc32c38c5722412268
Coomzy <ccoomzy+git@gmail.com>
2024-08-19 16:43:01 +0100

    Updated Code with non broken example and more buttons

--- code/StatAndLeaderboardTest.cs
+++ code/StatAndLeaderboardTest.cs
@@ -6,16 +6,21 @@ public class StatAndLeaderboardTest : Component
 	public const string STAT_EXAMPLE = "example";
 	public const string LEADERBOARD_EXAMPLE = "example";
 
+	public const string STAT_NON_BROKEN_EXAMPLE = "non-broken-example";
+	public const string LEADERBOARD_NON_BROKEN_EXAMPLE = "non-broken-example";
+
 	protected override void OnStart()
 	{
 		base.OnStart();
 
-		GetStat();
-		GetLeaderboard();
+		GetExampleStat();
+		GetNonBrokenExampleStat();
+		GetExampleLeaderboard();
+		GetNonBrokenExampleLeaderboard();
 	}
 
-	[Button( "Increment Stat" )]
-	void IncrementStat()
+	[Group ("Set/Increment Stat"), Button( "Increment Stat" )]
+	void IncrementExampleStat()
 	{
 		// This is what broke it
 		//Sandbox.Services.Stats.Increment( STAT_EXAMPLE, double.MaxValue);
@@ -23,31 +28,59 @@ public class StatAndLeaderboardTest : Component
 		Sandbox.Services.Stats.SetValue( STAT_EXAMPLE, 1);
 	}
 
-	[Button("Get Stat")]
-	async void GetStat()
+	[Group( "Set/Increment Stat" ), Button( "Increment Non Broken Stat" )]
+	void IncrementNonBrokenExampleStat()
 	{
-		var localStat = Sandbox.Services.Stats.LocalPlayer.Get( STAT_EXAMPLE );
-		var globalStat = Sandbox.Services.Stats.Global.Get( STAT_EXAMPLE );
+		Sandbox.Services.Stats.Increment( STAT_NON_BROKEN_EXAMPLE, 1 );
+	}
+
+	[Group( "Get Stat" ), Button( "Get Stat")]
+	void GetExampleStat()
+	{
+		GetStat( STAT_EXAMPLE );
+	}
+
+	[Group( "Get Stat" ), Button( "Get Non Broken Stat" )]
+	void GetNonBrokenExampleStat()
+	{
+		GetStat( STAT_NON_BROKEN_EXAMPLE );
+	}
+
+	[Group( "Get Leaderboard" ), Button( "Get Leaderboard" )]
+	void GetExampleLeaderboard()
+	{
+		GetLeaderboard( LEADERBOARD_EXAMPLE );
+	}
+
+	[Group( "Get Leaderboard" ), Button( "Get Non Broken Leaderboard" )]
+	void GetNonBrokenExampleLeaderboard()
+	{
+		GetLeaderboard( LEADERBOARD_NON_BROKEN_EXAMPLE );
+	}
+
+	async void GetStat(string statName)
+	{
+		var localStat = Sandbox.Services.Stats.LocalPlayer.Get( statName );
+		var globalStat = Sandbox.Services.Stats.Global.Get( statName );
 
 		var localRefreshTask = Sandbox.Services.Stats.LocalPlayer.Refresh();
 		var globalRefreshTask = Sandbox.Services.Stats.Global.Refresh();
 
 		await Task.WhenAll(localRefreshTask, globalRefreshTask);
 
-		Log.Info($"{STAT_EXAMPLE} local: {localStat.Value}, global: {globalStat.Value}");
+		Log.Info($"{statName} local: {localStat.Value}, global: {globalStat.Value}");
 	}
 
-	[Button("Get Leaderboard")]
-	async void GetLeaderboard()
+	async void GetLeaderboard(string leaderboardName)
 	{
-		var board = Sandbox.Services.Leaderboards.Get( LEADERBOARD_EXAMPLE );
+		var board = Sandbox.Services.Leaderboards.Get( leaderboardName );
 
 		board.MaxEntries = 10;
 		board.Group = "global";
 
 		await board.Refresh();
 
-		Log.Info($"Board: {board.DisplayName}, Group: {board.Title}, entries: {board.TotalEntries}");
+		Log.Info($"'{leaderboardName}' Board: {board.DisplayName}, Group: {board.Title}, entries: {board.TotalEntries}");
 
 		foreach (var e in board.Entries)
 		{
